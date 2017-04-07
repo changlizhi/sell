25c76d93937256b3ab3a174f4245288472be03ef
changlz <changlizhi@aliyun.com>
2017-04-07 17:34:08 +0800

    评论组件结束

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -28,7 +28,7 @@
       <ratingselect :select-type="selectType" :desc="desc" :ratings="ratings"></ratingselect>
       <div class="rating-wrapper">
         <ul>
-          <li v-for="rating in ratings" class="rating-item">
+          <li v-for="rating in ratings" v-show="needShow(rating.rateType,rating.text)" class="rating-item">
             <div class="avatar">
               <img width="28" height="28" :src="rating.avatar"/>
             </div>
@@ -36,12 +36,12 @@
               <h1 class="name">{{rating.username}}</h1>
               <div class="star-wrapper">
                 <star :size="24" :score="rating.score"></star>
-                <span class="delivery" v-show="rating.deliveryTime"></span>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
               </div>
               <p class="text">{{rating.text}}</p>
               <div class="recommend" v-show="rating.recommend.length">
                 <i class="icon-thumb_up"></i>
-                <span v-for="item in rating.recommend">{{item}}</span>
+                <span class="item" v-for="item in rating.recommend">{{item}}</span>
               </div>
               <div class="time">{{rating.rateTime | formatDate}}</div>
             </div>
@@ -63,6 +63,20 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    events: {
+      'ratingtype.select'(type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      'content.toggle'(onlyConent) {
+        this.onlyContent = onlyConent;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
       }
     },
     data() {
@@ -99,6 +113,18 @@
       star,
       split,
       ratingselect
+    },
+    methods: {
+      needShow(type, text) {
+        if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
+      }
     }
   };
 </script>
@@ -175,6 +201,63 @@
         display: flex
         padding: 18px 0
         borderbottom-1px(rgba(7, 17, 27, 0.1))
+        .avatar
+          flex: 0 0 28px
+          width: 28px
+          margin-right: 12px
+          img
+            border-radius: 50%
+        .content
+          position: relative
+          flex: 1
+          .name
+            margin-bottom: 4px
+            line-height: 12px
+            font-size: 10px
+            color: rgb(7, 17, 27)
+          .star-wrapper
+            margin-bottom: 6px
+            font-size: 0
+            .star
+              display: inline-block
+              margin-right: 6px
+              vertical-align: top
+            .delivery
+              display: inline-block
+              margin-right: 6px
+              vertical-align: top
+              line-height: 12px
+              font-size: 10px
+              color: rgb(147, 153, 159)
+
+          .text
+            margin-bottom: 8px
+            line-height: 18px
+            color: rgb(7, 17, 27)
+            font-size: 12px
+          .recommend
+            line-height: 16px
+            font-size: 0
+            .icon-thumb_up, .item
+              display: inline-block
+              margin: 0 8px 4px 0
+              font-size: 9px
+            .icon-thumb_up
+              color: rgb(0, 160, 220)
+            .item
+              padding-left: 6px
+              border: 1px solid rgba(7, 17, 27, 0.1)
+              border-radius: 1px
+              color: rgb(147, 153, 159)
+              background: #fff
+
+          .time
+            position: absolute
+            top: 0
+            right: 0
+            line-height: 12px
+            font-size: 10px
+            color: rgb(147, 153, 159)
 
   .efsss
     flex: 1
