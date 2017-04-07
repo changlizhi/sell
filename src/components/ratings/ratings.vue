fed194c05bfea7847973258d0f739418b43ba5f7
changlz <changlizhi@aliyun.com>
2017-04-07 15:49:09 +0800

    评价单页

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,8 +1,157 @@
 <template>
-  <div>component ratings</div>
+  <div class="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="rating-score">{{seller.score}}</h1>
+          <div class="rating-title">综合评分</div>
+          <div class="rating-rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="score-title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score-num">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="score-title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score-num">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="delivery-title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
+      <split></split>
+      <ratingselect :select-type="selectType" :desc="desc" :ratings="ratings"></ratingselect>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="rating in ratings" class="rating-item">
+            <div class="avatar">
+              <img :src="rating.avatar"/>
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime"></span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommand" v-show="rating.recommand.length">
+                <span class="icon-thumb_up"></span>
+                <span v-for="item in rating.recommand"></span>
+              </div>
+              <div class="time"></div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 <script type="text/ecmascript-6">
-  export default {};
+  import star from 'components/star/star.vue';
+  import split from 'components/split/split.vue';
+  import ratingselect from 'components/ratingselect/ratingselect.vue';
+  const ALL = 2;
+  const ERR_OK = 0;
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        ratings: [],
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true
+      };
+    },
+    created() {
+      this.$http.get('/api/ratings').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.ratings = response.data;
+        }
+      });
+    },
+    components: {
+      star,
+      split,
+      ratingselect
+    }
+  };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
+  .ratings
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview
+      display: flex
+      padding: 18px 0
+      .overview-left
+        flex: 0 0 137px
+        padding: 6px 0
+        border-right: 1px solid rgba(7, 17, 27, 0.1)
+        text-align: center
+        @media only screen and (max-width: 320px)
+          flex: 0 0 120px
+          width: 120px
+        .rating-score
+          margin-bottom: 6px
+          line-height: 28px
+          font-size: 24px
+          color: rgb(255, 153, 0)
+        .rating-title
+          margin-bottom: 8px
+          line-height: 12px
+          font-size: 12px
+          color: rgb(7, 17, 27)
+        .rating-rank
+          line-height: 10px
+          font-size: 10px
+          color: rgb(147, 153, 159)
+      .overview-right
+        flex: 1
+        padding-left: 6px 0 6px 24px
+        @media only screen and (max-width: 320px)
+          padding-left: 6px
+        .score-wrapper
+          margin-bottom: 8px
+          font-size: 0
+          .score-title
+            line-height: 18px
+            font-size: 12px
+            color: rgb(7, 17, 27)
+          .star
+            display: inline-block
+            margin: 0 12px
+            vertical-align: top
+          .score-num
+            display: inline-block
+            line-height: 18px
+            vertical-align: top
+            font-size: 12px
+            color: rgb(255, 153, 0)
+        .delivery-wrapper
+          font-size: 0
+          .delivery-title
+            line-height: 18px
+            font-size: 12px
+            color: rgb(7, 17, 27)
+          .delivery
+            margin-left: 12px
+            font-size: 12px
+            color: rgb(147, 153, 159)
+
+  .efsss
+    flex: 1
 </style>
