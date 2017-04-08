8c6edf9b3fd92be54b4911db09f91878bdd11335
changlz <changlizhi@aliyun.com>
2017-04-08 10:21:23 +0800

    切换页面时BScroll重新渲染watch不会初始化

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,8 +1,179 @@
-<template>
-  <div>component seller</div>
+<template xmlns:v-el="http://www.w3.org/1999/xhtml">
+  <div class="seller" v-el:seller>
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="seller-title">{{seller.name}}</h1>
+        <div class="desc borderbottom-1px">
+          <star :size="36" :score="seller.score"></star>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text">月售{{seller.sellCount}}单</span>
+        </div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryTime}}</span>分钟
+            </div>
+          </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="bulletin">
+        <h1 class="bulletin-title">公告与活动</h1>
+        <div class="content-wrapper borderbottom-1px">
+          <p class="content-in">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item borderbottom-1px" v-for="item in seller.supports">
+            <span class="item-icon" :class="classMap[seller.supports[$index].type]"></span>
+            <span class="item-text">{{ seller.supports[$index].description }}</span>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </div>
 </template>
 <script type="text/ecmascript-6">
-  export default {};
+  import star from 'components/star/star.vue';
+  import split from 'components/split/split.vue';
+  import BScroll from 'better-scroll';
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    components: {
+      star,
+      split
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    watch: {
+      'seller'() {
+        this._initScroll();
+      }
+    },
+    methods: {
+      _initScroll() {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$els.seller, {
+            click: true
+          });
+        }
+      }
+    }
+  };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl";
+  .seller
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview
+      padding: 18px
+      .seller-title
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7, 17, 27)
+        font-size: 14px
+      .desc
+        padding-bottom: 18px
+        borderbottom-1px(rgba(7, 17, 27, 0.1))
+        font-size: 0
+        .star
+          display: inline-block
+          margin-right: 8px
+          vertical-align: top
+        .text
+          display: inline-block
+          margin-right: 12px
+          line-height: 18px
+          vertical-align: top
+          font-size: 10px
+          color: rgb(77, 85, 93)
+      .remark
+        display: flex
+        padding-top: 18px
+        .block
+          flex: 1
+          text-align: center
+          border-right: 1px solid rgba(7, 17, 27, 0.1)
+          &:last-child
+            border: none
+          h2
+            margin-bottom: 4px
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .content
+            line-height: 24px
+            font-size: 10px
+            color: rgb(7, 17, 27)
+            .stress
+              font-size: 24px
+    .bulletin
+      padding: 18px 18px 0 18px
+      .bulletin-title
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7, 17, 27)
+        font-size: 14px
+      .content-wrapper
+        padding: 0 12px 16px 12px
+        borderbottom-1px(rgba(7, 17, 27, 0.1))
+        .content-in
+          line-height: 24px
+          font-size: 12px
+          color: rgb(240, 20, 20)
+
+      .supports
+        .support-item
+          padding: 16px 12px
+          borderbottom-1px(rgba(7, 17, 27, 0.1))
+          font-size: 0
+        .item-icon
+          display: inline-block
+          width: 16px
+          height: 16px
+          vertical-align: top
+          margin-right: 6px
+          background-size: 16px 16px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_4')
+          &.discount
+            bg-image('discount_4')
+          &.guarantee
+            bg-image('guarantee_4')
+          &.invoice
+            bg-image('invoice_4')
+          &.special
+            bg-image('special_4')
+        .item-text
+          line-height: 16px
+          font-size: 12px
+          color: rgb(7, 17, 27)
+
+  .fe
+    flex: 1
 </style>
